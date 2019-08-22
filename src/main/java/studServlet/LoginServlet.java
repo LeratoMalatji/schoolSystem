dc5bccee2d89660428ac9a035c062092b168fc92
LeratoMalatji <leratomalatji@ymail.com>
2019-08-22 19:33:49 +0200

    login servlet

---
+++ src/main/java/studServlet/LoginServlet.java
@@ -0,0 +1,29 @@
+package studServlet;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class LoginServlet extends HttpServlet {
+	
+	
+	public void service(HttpServletRequest req , HttpServletResponse res)
+	{
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+
+}
